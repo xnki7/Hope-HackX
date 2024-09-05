2ac4a8a6054ebcd5f5eac5aef982d51440eff4c6
xnki7 <codeinja7@gmail.com>
2024-09-05 16:31:12 +0530

    Create campaign form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import LandingLayout from "./layouts/landing-layout.jsx";
 import Homepage from "./pages/homepage.jsx";
+import ReportIncident from "./pages/reportIncident/reportIncident.jsx"
 function App() {
   return (
     <Routes>
       <Route element={<LandingLayout />} path="/">
         <Route element={<Homepage />} path="/home" />
+        <Route element={<ReportIncident />} path="/reportIncident" />
       </Route>
     </Routes>
   );
