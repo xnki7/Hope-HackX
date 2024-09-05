98f12a2519c791220fb92641bfb931214920a8f3
Sameer Madhav D <sameermadhav.5@gmail.com>
2024-09-06 04:18:13 +0530

    fetch

--- src/pages/campaigns-page/campaigns-page.jsx
+++ src/pages/campaigns-page/campaigns-page.jsx
@@ -1,14 +1,72 @@
 import CampaignCard from "../../components/campaign-card/campaign-card"
+import { useReadContract } from 'wagmi'
+import {contractAddress, contractAbi} from "../../../constants"
+import { useState, useEffect } from "react"
 
 const CampaignsPage = ({contractInstance}) => {
   const [campaigns, setCampaigns] = useState([]);
 
+  useEffect(() => {
+    getListedCampaigns()
+  }, [])
+  
+
   const getListedCampaigns = async()=>{
-    
+    const tx = useReadContract({
+    abi: contractAbi,
+    address: contractAddress,
+    functionName: 'getListedCampaigns',
+  })
+  setCampaigns(tx);
+  console.log(tx);
   }
 
-  const getMyCampaigns = async()=>{}
-  const getRemainingAmount = async(campaignId)=>{}
+  const fetchCampaignMetadata = async (campaignURI) => {
+    try {
+      const response = await axios.get(`https://ipfs.io/ipfs/${campaignURI}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching Campaign metadata:", error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    const fetchCampaignDetails = async () => {
+      const updatedCampaigns = await Promise.all(
+        campaigns.map(async (campaign) => {
+          const uri =  useReadContract({
+        contractAbi,
+        address: contractAddress,
+        functionName: 'getCampaignURI',
+        args:[campaign.campaignId]
+        })
+          const metadata = await fetchCampaignMetadata(uri);
+          console.log(
+            "Metadata for campaignId",
+            campaign.campaignId,
+            ":",
+            metadata
+          );
+          return { ...campaign, metadata };
+        })
+      );
+      console.log("Updated Campaigns:", updatedCampaigns);
+      setCampaigns(updatedCampaigns);
+    };
+
+    const fetchCampaigns = async () => {
+      if (campaigns.length > 0 && !campaigns[0].metadata) {
+        await fetchCampaignDetails();
+      }
+    };
+
+    fetchCampaigns();
+  }, [campaigns]);
+
+  const getRemainingAmount = async(campaignId)=>{
+
+  }
   const getRemainingTime = async(campaignId)=>{}
   const donateInCampaign = async(campaignId)=>{}
 
