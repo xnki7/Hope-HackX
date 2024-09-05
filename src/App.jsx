b677c40cebe2a1ff0f897836a4bd522cbeeeed25
Ankit Kumar <ankitkr2731@gmail.com>
2024-09-06 00:07:19 +0530

    chore: campaigns detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { Route, Routes } from "react-router-dom";
-import LandingLayout from "./layouts/landing-layout.jsx";
-import Homepage from "./pages/homepage.jsx";
-import CampaignsPage from "./pages/campaigns-page.jsx";
-import ReportIncident from "./pages/reportIncident/reportIncident.jsx"
+import { Route, Routes } from 'react-router-dom';
+import LandingLayout from './layouts/landing-layout.jsx';
+import Homepage from './pages/homepage/homepage.jsx';
+import CampaignsPage from './pages/campaigns-page/campaigns-page.jsx';
+import ReportIncident from './pages/reportIncident/reportIncident.jsx';
+import CampaignDetail from './pages/campaign-detail/campaign-detail.jsx';
 function App() {
-  return (
-    <Routes>
-      <Route element={<LandingLayout />} path="/">
-        <Route element={<Homepage />} path="/home" />
-        <Route element={<CampaignsPage />} path="/campaigns" />
-        <Route element={<ReportIncident />} path="/reportIncident" />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route element={<LandingLayout />} path="/">
+                <Route element={<Homepage />} path="/home" />
+                <Route element={<CampaignsPage />} path="/campaigns" />
+                <Route element={<CampaignDetail />} path="/c/:campaignId" />
+                <Route element={<ReportIncident />} path="/reportIncident" />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-import { nextui } from '@nextui-org/react'
+import { nextui } from '@nextui-org/react';
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-
-  ],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
-  plugins: [nextui({
-    prefix: 'nextui', // prefix for themes variables
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {},
+    },
+    darkMode: 'class',
+    plugins: [
+        nextui({
+            prefix: 'nextui', // prefix for themes variables
             addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
             defaultTheme: 'light', // default theme from the themes object
             defaultExtendTheme: 'light', // default theme to extend on custom themes
@@ -21,7 +21,12 @@ export default {
             themes: {
                 light: {
                     layout: {}, // light theme layout tokens
-                    colors: { primary: '#229799', secondary: '#48CFCB', content1: '#424242', background: '#F5F5F5' }, // light theme colors
+                    colors: {
+                        primary: '#229799',
+                        secondary: '#48CFCB',
+                        content1: '#424242',
+                        background: '#F5F5F5',
+                    }, // light theme colors
                 },
                 dark: {
                     layout: {}, // dark theme layout tokens
@@ -29,6 +34,24 @@ export default {
                 },
                 // ... custom themes
             },
-  })],
-}
-
+        }),
+        function ({ addUtilities }) {
+          addUtilities({
+              '.scrollbar-thin': {
+                  '&::-webkit-scrollbar': {
+                      width: '6px',
+                      marginTop: '2px',
+                      marginBottom: '2px',
+                  },
+                  '&::-webkit-scrollbar-thumb': {
+                      backgroundColor: '#d1d5db',
+                      borderRadius: '6px',
+                  },
+                  '&::-webkit-scrollbar-track': {
+                      backgroundColor: 'transparent',
+                  },
+              },
+          });
+      },
+    ],
+};
